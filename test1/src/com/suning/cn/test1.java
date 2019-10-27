050dc9c4eb765603646752eb437dc469c15bc56b
hf <123.com>
2019-10-27 15:09:55 +0800

    提交

---
+++ test1/src/com/suning/cn/test1.java
@@ -0,0 +1,11 @@
+package com.suning.cn;
+
+/**
+ * @author shkstart
+ * @create 2019-10-27
+ */
+public class test1 {
+    public static void main(String[] args) {
+        System.out.println("这个是测试");
+    }
+}
